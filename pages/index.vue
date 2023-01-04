cfbe44c1ca69583270a04965da2dd26919bc2a88
pang-vichhai <pangvichhai@gmail.com>
2023-01-04 17:03:12 +0700

    remove description

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <v-text-field outlined label="Add your new task"></v-text-field>
+    <v-form>
+      <v-text-field
+        v-model="task"
+        outlined
+        label="Add your new task"
+      ></v-text-field>
+
+    </v-form>
+
+    <div class="d-flex justify-end">
+      <v-btn @click="addTask" elevation="0" color="primary">Add New Task</v-btn>
+    </div>
 
     <v-list v-for="(task, i) in tasks" :key="i">
       <v-list-item>
@@ -10,13 +21,25 @@
         <v-list-item-content>
           <v-list-item-content> {{ task.title }} </v-list-item-content>
         </v-list-item-content>
-        <v-list-item-content>
-          <v-list-item-content> {{ task.date }} </v-list-item-content>
-        </v-list-item-content>
         <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="red">mdi-delete</v-icon>
-          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-on="on" v-bind="attrs">
+                <v-icon color="primary">mdi-file-find</v-icon>
+              </v-btn>
+            </template>
+            <span>Preview</span>
+          </v-tooltip>
+        </v-list-item-action>
+        <v-list-item-action>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-on="on" v-bind="attrs">
+                <v-icon color="red">mdi-delete</v-icon>
+              </v-btn>
+            </template>
+            <span>Delete</span>
+          </v-tooltip>
         </v-list-item-action>
       </v-list-item>
       <v-divider></v-divider>
@@ -29,6 +52,14 @@ import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters('task', { tasks: 'tasks' }),
+  },
+  date() {
+    return {
+      task: null,
+    }
+  },
+  methods: {
+    addTask() {},
   },
 }
 </script>
--- store/task/index.js
+++ store/task/index.js
@@ -1,9 +1,9 @@
 import getters from "./getters"
 export const state = () => ({
     tasks:[{
-        id:1,
         title:'First',
-        date:'12/12221'
+        desc:'',
+        date:''
     }]
 })
 
